740ba5c0ba823a21c1a4552251b723a7cac4f679
Nick Brown <nick@mesham.com>
2025-03-24 21:16:09 +0000

    Generation of rv out and compute for SIMD code

--- ftn/transforms/tenstorrent/convert_to_tt.py
+++ ftn/transforms/tenstorrent/convert_to_tt.py
@@ -7,6 +7,7 @@ from xdsl.dialects import memref, scf, omp
 import tenstorrent.dialects.data_movement as data_movement
 import tenstorrent.dialects.host as host
 import tenstorrent.dialects.circular_buffer as circular_buffer
+import tenstorrent.dialects.compute as compute
 from xdsl.ir import Operation, SSAValue, OpResult, Attribute, MLContext, Block, Region
 
 from xdsl.pattern_rewriter import (RewritePattern, PatternRewriter,
@@ -39,12 +40,16 @@ class GatherMemoryPassedToDevice(Visitor):
       self.memory_types.append(map_var.type.element_type)
       self.references.append(target_op.region.block.args[idx])
 
-class GatherFor(Visitor):
+class GatherComputeLoop(Visitor):
   def __init__(self):
     self.for_op=None
+    self.simd_loop=None
 
   def traverse_for(self, for_op: scf.For):
     self.for_op=for_op
+
+  def traverse_s_i_m_d_loop_op(self, simdloop: omp.SIMDLoopOp):
+    self.simd_loop=simdloop
 
 @dataclass(frozen=True)
 class ConvertToTT(ModulePass):
@@ -53,18 +58,11 @@ class ConvertToTT(ModulePass):
   """
   name = 'convert-to-tt'
 
-  def generate_device(self, module, memory_type, references, for_op):
-    assert for_op is not None
-
-    # For each memref first passed in is memory addresses, then bank ids, and then memory sizes (number elements)
-
-    arg_types=[uint32]*len(memory_type)*3
-
-    new_block = Block(arg_types=arg_types)
-
-    ops=[]
-    ssa_res=[]
+  def generate_data_in(self, module, memory_type, references, new_block, generate_writeback=False):
     write_back_ops=[]
+    push_back_ops=[]
+    one_c=arith.Constant.from_int_and_width(1, 32)
+    new_block.add_op(one_c)
     for idx, t in enumerate(memory_type):
       is_deref_memref=isa(t, builtin.MemRefType)
       if is_deref_memref:
@@ -75,6 +73,7 @@ class ConvertToTT(ModulePass):
       dm_op.results[0].name_hint = f"src{idx}_dram_noc_addr"
 
       const_op=arith.Constant.from_int_and_width(idx, 32)
+      cb_reserve_op=circular_buffer.CBReserveBack(const_op.results[0], one_c)
       cb_op=circular_buffer.CBGetWritePointer(const_op.results[0])
       cb_op.results[0].name_hint = "l1_write_addr_in"+str(idx)
 
@@ -88,8 +87,7 @@ class ConvertToTT(ModulePass):
       conversion_op=builtin.UnrealizedConversionCastOp.get([cb_op.results[0]], [target_memref])
       conversion_op.results[0].name_hint = f"src{idx}_data"
 
-      new_block.add_ops([dm_op, const_op, cb_op, data_type_byte_width, dt_width_conversion_op, mem_size_bytes_op, read_op, conversion_op])
-      ssa_res.append(conversion_op.results[0])
+      new_block.add_ops([dm_op, const_op, cb_reserve_op, cb_op, data_type_byte_width, dt_width_conversion_op, mem_size_bytes_op, read_op, conversion_op])
 
       for use in references[idx].uses:
         if isa(use.operation, memref.Load):
@@ -98,10 +96,166 @@ class ConvertToTT(ModulePass):
             use.operation.detach()
             use.operation.results[0].replace_by(conversion_op.results[0])
 
-      write_op=data_movement.DMNocAsyncWrite(cb_op.results[0], dm_op.results[0], mem_size_bytes_op)
-      write_back_ops.append(write_op)
+      push_back_ops.append(circular_buffer.CBPushBack(const_op.results[0], one_c))
+
+      if generate_writeback:
+        write_op=data_movement.DMNocAsyncWrite(cb_op.results[0], dm_op.results[0], mem_size_bytes_op)
+        write_back_ops.append(write_op)
 
     new_block.add_op(data_movement.DMNocAsyncReadBarrier())
+
+    return push_back_ops, write_back_ops
+
+  def generate_device_rv_in(self, module, memory_type, references):
+    # For each memref first passed in is memory addresses, then bank ids, and then memory sizes (number elements)
+    arg_types=[uint32]*len(memory_type)*3
+    new_block = Block(arg_types=arg_types)
+
+    push_back_ops, write_back_ops=self.generate_data_in(module, memory_type, references, new_block)
+
+    new_block.add_ops(push_back_ops)
+    new_block.add_op(func.Return())
+
+    body = Region()
+    body.add_block(new_block)
+
+    func_op = func.FuncOp(
+      "kernel_main",
+      builtin.FunctionType.from_lists(arg_types, []),
+      body
+    )
+
+    new_module=builtin.ModuleOp([func_op], {"kernel_type": builtin.StringAttr("data_in")})
+
+    return new_module
+
+  def generate_device_rv_out(self, module, memory_type, references):
+     # For each memref first passed in is memory addresses, then bank ids, and then memory sizes (number elements)
+    arg_types=[uint32]*len(memory_type)*3
+    new_block = Block(arg_types=arg_types)
+
+    pop_front_ops=[]
+
+    one_c=arith.Constant.from_int_and_width(1, 32)
+    new_block.add_op(one_c)
+    for idx, t in enumerate(memory_type):
+      is_deref_memref=isa(t, builtin.MemRefType)
+      if is_deref_memref:
+        element_type=t.element_type
+      else:
+        element_type=t
+      dm_op=data_movement.DMGetNocAddrFromBankId(builtin.IntegerAttr.from_int_and_width(1, 1), new_block.args[len(memory_type)+idx], new_block.args[idx])
+      dm_op.results[0].name_hint = f"src{idx}_dram_noc_addr"
+
+      const_op=arith.Constant.from_int_and_width(idx, 32)
+      cb_waitfront_op=circular_buffer.CBWaitFront(const_op.results[0], one_c)
+      cb_op=circular_buffer.CBGetReadPointer(const_op.results[0])
+      cb_op.results[0].name_hint = "l1_read_addr_in"+str(idx)
+
+      assert element_type.width.data % 8 == 0
+      data_type_byte_width=arith.Constant.from_int_and_width(int(element_type.width.data / 8), 32)
+      dt_width_conversion_op=builtin.UnrealizedConversionCastOp.get([data_type_byte_width.results[0]], [uint32])
+      mem_size_bytes_op=arith.Muli(dt_width_conversion_op, new_block.args[(len(memory_type)*2)+idx])
+      read_op=data_movement.DMNocAsyncWrite(cb_op.results[0], dm_op.results[0], mem_size_bytes_op)
+
+      new_block.add_ops([dm_op, const_op, cb_waitfront_op, cb_op, data_type_byte_width, dt_width_conversion_op, mem_size_bytes_op, read_op])
+
+      pop_front_ops.append(circular_buffer.CBPopFront(const_op.results[0], one_c))
+
+    new_block.add_op(data_movement.DMNocAsyncWriteBarrier())
+    new_block.add_ops(pop_front_ops)
+
+    new_block.add_op(func.Return())
+
+    body = Region()
+    body.add_block(new_block)
+
+    func_op = func.FuncOp(
+      "kernel_main",
+      builtin.FunctionType.from_lists(arg_types, []),
+      body
+    )
+
+    new_module=builtin.ModuleOp([func_op], {"kernel_type": builtin.StringAttr("data_out")})
+
+    return new_module
+
+
+  def generate_device_compute(self, module, memory_type, references, simd_loop):
+    assert simd_loop is not None
+
+    simd_loop.detach()
+    f_lb_conv=simd_loop.lowerBound[0].owner.detach()
+    f_ub_add=simd_loop.upperBound[0].owner.detach()
+    f_step=simd_loop.step[0].owner.detach()
+
+    one_c=arith.Constant.from_int_and_width(1, 32)
+
+    compute_ops=[one_c]
+    for idx, t in enumerate(memory_type):
+      const_op=arith.Constant.from_int_and_width(idx, 32)
+      cb_waitfront_op=circular_buffer.CBWaitFront(const_op.results[0], one_c)
+      compute_ops+=[const_op, cb_waitfront_op]
+
+    cb1_i_op=arith.Constant.from_int_and_width(0, 32)
+    cb1_op=builtin.UnrealizedConversionCastOp.get([cb1_i_op], [uint32])
+    cb2_i_op=arith.Constant.from_int_and_width(1, 32)
+    cb2_op=builtin.UnrealizedConversionCastOp.get([cb2_i_op], [uint32])
+    const_16_i_op=arith.Constant.from_int_and_width(16, 32)
+    const_16_op=builtin.UnrealizedConversionCastOp.get([const_16_i_op], [uint32])
+    false_op=arith.Constant.from_int_and_width(0, 1)
+
+    binary_init_common_op=compute.BinaryOpInitCommon(cb1_op, cb2_op, const_16_op)
+    add_init_op=compute.AddInit(cb1_op, cb2_op, false_op)
+
+    zero_i_op=arith.Constant.from_int_and_width(0, 32)
+    zero_op=builtin.UnrealizedConversionCastOp.get([zero_i_op], [uint32])
+    dst_i_op=arith.Constant.from_int_and_width(0, 32)
+    dst_op=builtin.UnrealizedConversionCastOp.get([dst_i_op], [uint32])
+    acquire_regs_op=compute.RegsAcquire()
+    add_tile_op=compute.Add(cb1_op, cb2_op, zero_op, zero_op, dst_op)
+    commit_regs_op=compute.RegsCommit()
+
+    cb_out_i_op=arith.Constant.from_int_and_width(2, 32)
+    cb_out_op=builtin.UnrealizedConversionCastOp.get([cb_out_i_op], [uint32])
+    wait_regs_op=compute.RegsWait()
+    pack_tile_op=compute.PackTile(builtin.IntegerAttr.from_index_int_value(0), zero_op, cb_out_op, zero_op)
+    release_regs_op=compute.RegsRelease()
+
+    compute_ops+=[cb1_i_op, cb1_op, cb2_i_op, cb2_op, const_16_i_op, const_16_op, false_op, binary_init_common_op,
+                    add_init_op, zero_i_op, zero_op, dst_i_op, dst_op, acquire_regs_op,
+                    add_tile_op, commit_regs_op, cb_out_i_op, cb_out_op, wait_regs_op, pack_tile_op, release_regs_op]
+
+    for idx, t in enumerate(memory_type):
+      const_op=arith.Constant.from_int_and_width(idx, 32)
+      cb_popfront_op=circular_buffer.CBPopFront(const_op.results[0], one_c)
+      compute_ops+=[const_op, cb_popfront_op]
+
+    new_block=Block()
+    new_block.add_ops(compute_ops)
+    new_block.add_op(func.Return())
+
+    body = Region()
+    body.add_block(new_block)
+
+    func_op = func.FuncOp(
+      "kernel_main",
+      builtin.FunctionType.from_lists([], []),
+      body
+    )
+
+    new_module=builtin.ModuleOp([func_op], {"kernel_type": builtin.StringAttr("compute")})
+
+    return new_module
+
+
+  def generate_device_all_on_rv(self, module, memory_type, references, for_op):
+    assert for_op is not None
+    # For each memref first passed in is memory addresses, then bank ids, and then memory sizes (number elements)
+    arg_types=[uint32]*len(memory_type)*3
+    new_block = Block(arg_types=arg_types)
+
+    push_back_ops, write_back_ops=self.generate_data_in(module, memory_type, references, new_block, True)
 
     for_op.detach()
     f_lb_conv=for_op.lb.owner
@@ -168,6 +322,9 @@ class ConvertToTT(ModulePass):
     new_block.add_op(for_loop)
     new_block.add_ops(write_back_ops)
     new_block.add_op(data_movement.DMNocAsyncWriteBarrier())
+    # This is a bit of a strange way of doing it, but cleans up. We reserve
+    # for each CB and use its write pointer in memory, here clean them up
+    new_block.add_ops(push_back_ops)
     new_block.add_op(func.Return())
 
     body = Region()
@@ -319,13 +476,24 @@ class ConvertToTT(ModulePass):
     memref_visitor=GatherMemoryPassedToDevice()
     memref_visitor.traverse(module)
 
-    for_visitor=GatherFor()
+    for_visitor=GatherComputeLoop()
     for_visitor.traverse(module)
 
     device_func_visitor=GatherDeviceFunctionArgs()
     device_func_visitor.traverse(module)
 
-    target_func_op=self.generate_device(module, memref_visitor.memory_types, memref_visitor.references, for_visitor.for_op)
+    if for_visitor.for_op is not None:
+      # A plain for loop, just put this on the RV data in core
+      device_in_kernel_module=self.generate_device_all_on_rv(module, memref_visitor.memory_types, memref_visitor.references, for_visitor.for_op)
+      device_out_kernel_module=None
+      device_compute_kernel_module=None
+    elif for_visitor.simd_loop is not None:
+      # This is a simd loop, therefore need to split across the Tensix core
+      device_in_kernel_module=self.generate_device_rv_in(module, memref_visitor.memory_types, memref_visitor.references)
+      device_out_kernel_module=self.generate_device_rv_out(module, memref_visitor.memory_types, memref_visitor.references)
+      device_compute_kernel_module=self.generate_device_compute(module, memref_visitor.memory_types, memref_visitor.references, for_visitor.simd_loop)
+    else:
+      assert False
 
     host_func_op=self.generate_host(module, memref_visitor.memory_types, device_func_visitor.args)
 
@@ -337,5 +505,10 @@ class ConvertToTT(ModulePass):
     module.regions[0].move_blocks(containing_mod.regions[0])
 
     block = Block()
-    block.add_ops([target_func_op, host_func_op])
+    block.add_op(device_in_kernel_module)
+    if device_out_kernel_module is not None:
+      block.add_op(device_out_kernel_module)
+    if device_compute_kernel_module is not None:
+      block.add_op(device_compute_kernel_module)
+    block.add_op(host_func_op)
     module.regions[0].add_block(block)
