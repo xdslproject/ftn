58a649cb77928017b3c8596c0f4acb625e70b9b3
Gabriel Rodriguez-Canal <gabriel.rodcanal@ed.ac.uk>
2025-08-04 20:39:34 +0200

    Dereference in the HLS pass not necessary anymore, as it is processed earlier in the pipeline. Also, remove attributes from the module, since they are incompatible with the downgrading to LLVM v7

--- ftn/transforms/fpga/target_to_hls.py
+++ ftn/transforms/fpga/target_to_hls.py
@@ -19,24 +19,9 @@ from xdsl.dialects import builtin, func, llvm, arith
 from ftn.util.visitor import Visitor
 from xdsl.rewriter import InsertPoint
 
-@dataclass
-class TargetFuncToHLS(RewritePattern):
-    target_funcs : list[func.FuncOp] = field(default_factory=list)
-
-    def deref_args(self, func_op: func.FuncOp, rewriter : PatternRewriter):
-        """Dereference the arguments of a function operation."""
-        new_input_types = []
-
-        for arg in func_op.body.block.args:
-            if isa(arg.type, builtin.MemRefType):
-                deref_type = arg.type.element_type
-                func_op.replace_argument_type(arg, deref_type, rewriter)
-                new_input_types.append(deref_type)
-
-    def forward_map_info(self, map_info: omp.MapInfoOp, rewriter: PatternRewriter):
-        map_info.omp_ptr.replace_by(map_info.var_ptr)
-
-    def deref_scalar_memops(self, scalar_ssa: SSAValue, rewriter: PatternRewriter):
+class DerefMemrefs:
+    @staticmethod
+    def deref_scalar_memops(scalar_ssa: SSAValue, rewriter: PatternRewriter):
         for use in scalar_ssa.uses:
             if isinstance(use.operation, memref.LoadOp):
                 load_op = use.operation
@@ -47,7 +32,8 @@ class TargetFuncToHLS(RewritePattern):
             elif isinstance(use.operation, memref.StoreOp):
                 rewriter.erase_op(use.operation)
 
-    def deref_memref_memops(self, memref_ssa: SSAValue, rewriter: PatternRewriter):
+    @staticmethod
+    def deref_memref_memops(memref_ssa: SSAValue, rewriter: PatternRewriter):
         for use in memref_ssa.uses:
             if isinstance(use.operation, memref.LoadOp):
                 # The first load was used to load the pointer to the array. The index to retrieve an element from the array is applied 
@@ -74,18 +60,25 @@ class TargetFuncToHLS(RewritePattern):
                         rewriter.erase_op(array_store_op)
                         rewriter.erase_op(ptr_load_op)
 
+    @staticmethod
+    def deref_args(func_op: func.FuncOp, rewriter : PatternRewriter):
+        """Dereference the arguments of a function operation."""
+        new_input_types = []
 
-    def remove_target(self, target_op: omp.TargetOp, target_func: func.FuncOp, rewriter: PatternRewriter):
+        for arg in func_op.body.block.args:
+            if isa(arg.type, builtin.MemRefType):
+                deref_type = arg.type.element_type
+                func_op.replace_argument_type(arg, deref_type, rewriter)
+                new_input_types.append(deref_type)
+
+class RemoveOps:
+    @staticmethod
+    def remove_target(target_op: omp.TargetOp, target_func: func.FuncOp, rewriter: PatternRewriter):
         """Remove the target operation from the module."""
         for operand in target_op.map_vars:
             operand_idx = target_op.operands.index(operand)
             block_arg = target_op.region.block.args[operand_idx]
             block_arg.replace_by(operand)
-
-            if not isinstance(operand.type, builtin.MemRefType):
-                self.deref_scalar_memops(operand, rewriter)
-            else:
-                self.deref_memref_memops(operand, rewriter)
 
         target_op_terminator = target_op.region.block.last_op
         assert target_op_terminator
@@ -98,7 +91,8 @@ class TargetFuncToHLS(RewritePattern):
 
         rewriter.erase_op(target_op)
 
-    def remove_remaining_omp_ops(self, target_func: func.FuncOp, rewriter: PatternRewriter):
+    @staticmethod
+    def remove_remaining_omp_ops(target_func: func.FuncOp, rewriter: PatternRewriter):
         """Remove any remaining OpenMP operations in the target function."""
         for op in target_func.walk():
             if isinstance(op, omp.MapInfoOp):
@@ -107,6 +101,15 @@ class TargetFuncToHLS(RewritePattern):
         for op in target_func.walk():
             if isinstance(op, omp.MapBoundsOp):
                 rewriter.erase_op(op)
+
+    @staticmethod
+    def forward_map_info(map_info: omp.MapInfoOp, rewriter: PatternRewriter):
+        map_info.omp_ptr.replace_by(map_info.var_ptr)
+
+
+@dataclass
+class TargetFuncToHLS(RewritePattern):
+    target_funcs : list[func.FuncOp] = field(default_factory=list)
 
     @op_type_rewrite_pattern
     def match_and_rewrite(self, module: builtin.ModuleOp, rewriter: PatternRewriter, /):
@@ -117,16 +120,15 @@ class TargetFuncToHLS(RewritePattern):
         target_func = [op for op in module.walk() if isinstance(op, func.FuncOp) and op.sym_name.data == target_name][0]
         self.target_funcs.append(target_func)
 
-        self.deref_args(target_func, rewriter)
         for map_info in target_func.walk():
             if not isinstance(map_info, omp.MapInfoOp):
                 continue
 
-            self.forward_map_info(map_info, rewriter)
+            RemoveOps.forward_map_info(map_info, rewriter)
 
         target_op = [op for op in target_func.walk() if isinstance(op, omp.TargetOp)][0]
-        self.remove_target(target_op, target_func, rewriter)
-        self.remove_remaining_omp_ops(target_func, rewriter)
+        RemoveOps.remove_target(target_op, target_func, rewriter)
+        RemoveOps.remove_remaining_omp_ops(target_func, rewriter)
 
 
 @dataclass(frozen=True)
@@ -155,3 +157,4 @@ class TargetToHLSPass(ModulePass):
         module.body.detach_block(module.body.block)
         module_block.erase()
         module.body.add_block(Block(target_funcs))
+        module.attributes = {}
