e16495000135bce054aa2ea1a9da212bde9a4b01
Gabriel Rodriguez-Canal <gabriel.rodcanal@ed.ac.uk>
2025-08-07 13:29:16 +0100

    extract-target now extracts the body of the kernel_create operation into a new function (previously the omp.target operation was kept inside the function)

--- ftn/transforms/extract_target.py
+++ ftn/transforms/extract_target.py
@@ -1,21 +1,15 @@
-from abc import ABC
-from ast import Module
-from hmac import new
-from typing import TypeVar, cast
 from dataclasses import dataclass, field
-import itertools
-from xdsl.utils.hints import isa
-from xdsl.dialects import memref, scf, omp
 from xdsl.context import Context
-from xdsl.ir import Operation, SSAValue, OpResult, Attribute, Block, Region
+from xdsl.ir import Operation, Block, Region
 
 from xdsl.pattern_rewriter import (RewritePattern, PatternRewriter,
                                    op_type_rewrite_pattern,
                                    PatternRewriteWalker,
                                    GreedyRewritePatternApplier)
 from xdsl.passes import ModulePass
-from xdsl.dialects import builtin, func, llvm, arith
+from xdsl.dialects import builtin, func
 from xdsl.rewriter import InsertPoint
+from ftn.dialects import device
 
 @dataclass
 class RewriteTarget(RewritePattern):
@@ -23,9 +17,9 @@ class RewriteTarget(RewritePattern):
   target_ops: list[Operation] = field(default_factory=list)
 
   @op_type_rewrite_pattern
-  def match_and_rewrite(self, op: omp.TargetOp, rewriter: PatternRewriter, /):
+  def match_and_rewrite(self, op: device.KernelCreate, rewriter: PatternRewriter, /):
     arg_types = []
-    for var in op.has_device_addr_vars:
+    for var_idx,var in enumerate(op.mapped_data):
       assert isinstance(var.type, builtin.MemRefType)
       var_type = var.type
       arg_types.append(var_type)
@@ -33,20 +27,35 @@ class RewriteTarget(RewritePattern):
     # The target function is extracted to a function in a different module. This function will 
     # be called in the original module instead. The function signature is necessary to have the 
     # function in the symbol table.
-    call_dev_func = func.CallOp("tt_device", op.has_device_addr_vars, [])
-    dev_func_signature = func.FuncOp.external("tt_device", arg_types, [])
+    call_dev_func = func.CallOp("tt_device", op.mapped_data, [device.KernelHandle()])
+    dev_func_signature = func.FuncOp.external("tt_device", arg_types, [device.KernelHandle()])
     rewriter.insert_op(call_dev_func, InsertPoint.before(op))
     rewriter.insert_op(dev_func_signature, InsertPoint.at_start(self.module.body.block))
 
-    dev_func_block = Block(arg_types=arg_types)
-    op.detach()
+    dev_func_block = op.body.block
 
-    rewriter.insert_op(op, InsertPoint.at_start(dev_func_block))
-    rewriter.insert_op(func.ReturnOp(), InsertPoint.at_end(dev_func_block))
-    for block_arg,operand in zip(dev_func_block.args, op.has_device_addr_vars):
+    # Fix type of the block arguments (there is a mismatch between mapped_data and block args)
+    n_args = len(dev_func_block.args)
+    for block_arg, arg_type in zip(dev_func_block.args, arg_types):
+      new_block_arg = dev_func_block.insert_arg(arg_type, len(dev_func_block.args))
+      block_arg.replace_by(new_block_arg)
+
+    for arg in dev_func_block.args[:n_args]:
+      rewriter.erase_block_argument(arg)
+
+    op.res.replace_by(call_dev_func.results[0])
+    op.body.detach_block(op.body.block)
+    rewriter.erase_matched_op()
+    assert dev_func_block.last_op is not None, "The last operation in the device function block must not be None"
+    rewriter.erase_op(dev_func_block.last_op)
+
+    dev_func_body = Region([dev_func_block])
+
+    rewriter.insert_op(func.ReturnOp(), InsertPoint.at_end(dev_func_body.block))
+    for block_arg,operand in zip(dev_func_block.args, op.mapped_data):
       operand.replace_by_if(block_arg, lambda use: use.operation == op)
 
-    dev_func = func.FuncOp.from_region("tt_device", arg_types, [], Region(dev_func_block))
+    dev_func = func.FuncOp.from_region("tt_device", arg_types, [], dev_func_body)
 
 
     self.target_ops = [dev_func]
