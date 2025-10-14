ba33e508573e6cd9c79ec5b59b021238c3fb557a
dk949 <56653556+dk949@users.noreply.github.com>
2025-10-14 14:34:49 +0100

    expose lift-omp-to-tensor

--- ftn/tools/ftn_opt.py
+++ ftn/tools/ftn_opt.py
@@ -12,6 +12,7 @@ from ftn.transforms.fpga.target_to_hls import TargetToHLSPass
 from ftn.transforms.lower_omp_target_data import LowerOmpTargetDataPass
 from ftn.transforms.apply_target_config import ApplyTargetConfig
 from ftn.transforms.omp_target_to_kernel import OmpTargetToKernelPass
+from ftn.transforms.lift_omp_to_tensor import LiftOmpToTensorPass
 # from ftn.transforms.isolate_target import IsolateTarget
 # from psy.extract_stencil import ExtractStencil
 # from ftn.transforms.tenstorrent.convert_to_tt import ConvertToTT
@@ -38,6 +39,7 @@ class FtnOptMain(xDSLOptMain):
         self.register_pass("lower-omp-target-data", lambda: LowerOmpTargetDataPass)
         self.register_pass("apply-target", lambda: ApplyTargetConfig)
         self.register_pass("omp-target-to-kernel", lambda: OmpTargetToKernelPass)
+        self.register_pass("lift-omp-to-tensor", lambda: LiftOmpToTensorPass)
         # self.register_pass("isolate-target", lambda: IsolateTarget)
         # self.register_pass("convert-to-tt", lambda: ConvertToTT)
 
